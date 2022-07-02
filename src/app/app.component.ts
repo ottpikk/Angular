c8dc742894c71690fb0d39a55df459733fdb7b8d
Ott Pikk <ott@karmai.eu>
2022-07-02 10:32:48 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sda-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'javaremoteee15-first';
+}
